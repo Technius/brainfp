0fd5961e74fff4edd3ab562c8f8a1d77d099751f
Technius <techniux@gmail.com>
2016-07-17 15:36:15 -0700

    Start work on pure evaluation

--- src/main/scala/co/technius/brainfp/Interpreter.scala
+++ src/main/scala/co/technius/brainfp/Interpreter.scala
@@ -27,4 +27,38 @@ object Interpreter {
     doEval(commands)
     output.toString
   }
+
+  def step(state: State, next: Seq[Command]): (State, EvalStep) = {
+    if (next.isEmpty) {
+      state -> Finish
+    } else {
+      val ptr = state.ptr
+      val stack = state.stack
+      next.head match {
+        case RightShiftPointer => state.copy(ptr = ptr + 1) -> Resume
+        case LeftShiftPointer  => state.copy(ptr = ptr - 1) -> Resume
+        case IncrementCell =>
+          val updStack = stack.updated(ptr, (stack(ptr) + 1).toByte)
+          state.copy(stack = updStack) -> Resume
+        case DecrementCell =>
+          val updStack = stack.updated(ptr, (stack(ptr) - 1).toByte)
+          state.copy(stack = updStack) -> Resume
+        case OutputCharacter => state -> Output(stack(ptr).toChar)
+        case InputCharacter =>
+          state -> AwaitInput
+        case Loop(commands) =>
+          state -> (if (stack(ptr) > 0) Execute(commands) else Resume)
+      }
+    }
+  }
+
 }
+
+sealed trait EvalStep
+case class Execute(commands: Seq[Command]) extends EvalStep
+case class Output(output: Char) extends EvalStep
+case object AwaitInput extends EvalStep
+case object Resume extends EvalStep
+case object Finish extends EvalStep
+
+case class State(stack: Seq[Byte], ptr: Int)
